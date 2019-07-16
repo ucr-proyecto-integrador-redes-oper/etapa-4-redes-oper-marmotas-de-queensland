b3c645b375e860f98218f1b4afa6251dce08c726
Rodolfo <jrcc28@hotmail.com>
2019-07-15 18:07:30 -0600

    se agrego un parametro mas

--- code/source/orange_instantiator.cpp
+++ code/source/orange_instantiator.cpp
@@ -1,6 +1,6 @@
 #include "orange.h"
 #include <string.h>
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -9,18 +9,19 @@ using namespace std;
 
 int main(int argc, char* argv[]){
   if(argc < 6){
-    cout << "Usage: portNaranja, portAzul, pathcsv, ipDer, portDer." << endl;
+    cout << "Usage: portNaranja, cantidadAzules, portAzul, pathcsv, ipDer, portDer." << endl;
   }else{
     int portNaranja = atoi(argv[1]);
-    int portAzul = atoi(argv[2]);
-    char* ipDer = argv[4];
-    int portDer = atoi(argv[5]);
-    char* ipIzq = argv[6];
-    int portIzq = atoi(argv[7]);
+    int cantidadAzules = atoi(argv[2]);
+    int portAzul = atoi(argv[3]);
+    char* ipDer = argv[5];
+    int portDer = atoi(argv[6]);
+    char* ipIzq = argv[7];
+    int portIzq = atoi(argv[8]);
     int a;
     //cout << "ingrese key" << endl;
     //cin >> a;
-    Naranja naranja(portNaranja,0,argv[3],ipDer,portDer);
+    Naranja naranja(portNaranja,cantidadAzules,portAzul,argv[4],ipDer,portDer);
     cout << "ingrese "<<endl;
     cin >> a;
     naranja.iniciar();
